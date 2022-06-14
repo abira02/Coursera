b8c64281befd372f99149154622c1de16634ee99
abira02 <99014671+abira02@users.noreply.github.com>
2022-06-14 17:15:06 +0530

    Update findJane.py

--- Course_2/Week_6/findJane.py
+++ Course_2/Week_6/findJane.py
@@ -2,5 +2,5 @@
 > oldFiles.txt
 files=$(grep " jane " ../data/list.txt | cut -d' ' -f3)
 for f in $files; do
-   if test -e '/home/<username>'$f; then echo $f>>oldFiles.txt; else echo'File does not exist';fi
+   if test -e '/home/<username>'$f; then echo $f>>oldFiles.txt; else echo 'File does not exist';fi
 done
