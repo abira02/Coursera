be42786a2b703e062fca8f0ddba4a9e36e5d27d2
abira02 <99014671+abira02@users.noreply.github.com>
2022-08-08 16:19:46 +0530

    Update changeJane.py

--- Course_2/Week_6/changeJane.py
+++ Course_2/Week_6/changeJane.py
@@ -7,5 +7,5 @@ for file in myfile.readlines():
   old_name = file.strip()
   new_name = old_name.replace('jane','jdoe')
   print(new_name)
-  subprocess.run(["mv", '/home/<username>'+old_name, '/home/<username>'+new_name])
+  subprocess.run(["mv", old_name, new_name])
 myfile.close()
